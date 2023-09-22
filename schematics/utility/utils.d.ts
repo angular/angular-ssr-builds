856ca833e1ecbc20d5d1edf9df7a230b36745ca6
Angular Builds <circleci@angular.io>
2023-09-22 15:42:08 +0000

    fc68dcaa0 refactor(@angular/ssr): remove code that sets `initialNavigation: enabledBlocking`

--- schematics/utility/utils.d.ts
+++ schematics/utility/utils.d.ts
@@ -7,16 +7,5 @@
  */
 import { workspaces } from '@angular-devkit/core';
 import { Tree } from '@angular-devkit/schematics';
-import * as ts from 'typescript';
 export declare function getProject(host: Tree, projectName: string): Promise<workspaces.ProjectDefinition>;
-export declare function stripTsExtension(file: string): string;
 export declare function getOutputPath(host: Tree, projectName: string, target: 'server' | 'build'): Promise<string>;
-export declare function findImport(sourceFile: ts.SourceFile, moduleName: string, symbolName: string): ts.NamedImports | null;
-export type Import = {
-    name: string;
-    importModule: string;
-    node: ts.ImportDeclaration;
-};
-/** Gets import information about the specified identifier by using the Type checker. */
-export declare function getImportOfIdentifier(typeChecker: ts.TypeChecker, node: ts.Identifier): Import | null;
-export declare function addInitialNavigation(node: ts.CallExpression): ts.CallExpression;

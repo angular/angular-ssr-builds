37c45862be259ff4d062dcf4ccca8faa944e9728
Angular Builds <circleci@angular.io>
2023-09-25 18:24:49 +0000

    741cca73c feat(@schematics/angular): add `ng new --ssr`

--- schematics/ng-add/index.d.ts
+++ schematics/ng-add/index.d.ts
@@ -5,6 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { Rule } from '@angular-devkit/schematics';
-import { Schema as AddServerOptions } from './schema';
-export default function (options: AddServerOptions): Rule;
+import { Schema as SSROptions } from './schema';
+declare const _default: (options: SSROptions) => import("@angular-devkit/schematics").Rule;
+export default _default;

37ee864ed976c834135e4380711f4e9d2e60a1a3
Angular Builds <circleci@angular.dev>
2024-11-05 14:04:39 +0000

    47bbaca docs: replace `@note` with `@remarks`

--- node/index.d.ts
+++ node/index.d.ts
@@ -9,7 +9,7 @@ import { Type } from '@angular/core';
  * Manages Angular server applications (including localized ones), handles rendering requests,
  * and optionally transforms index HTML before rendering.
  *
- * @note This class should be instantiated once and used as a singleton across the server-side
+ * @remarks This class should be instantiated once and used as a singleton across the server-side
  * application to ensure consistent handling of rendering requests and resource management.
  *
  * @developerPreview
@@ -24,7 +24,7 @@ export declare class AngularNodeAppEngine {
      * @param requestContext - Optional context for rendering, such as metadata associated with the request.
      * @returns A promise that resolves to the resulting HTTP response object, or `null` if no matching Angular route is found.
      *
-     * @note A request to `https://www.example.com/page/index.html` will serve or render the Angular route
+     * @remarks A request to `https://www.example.com/page/index.html` will serve or render the Angular route
      * corresponding to `https://www.example.com/page`.
      */
     handle(request: IncomingMessage, requestContext?: unknown): Promise<Response | null>;

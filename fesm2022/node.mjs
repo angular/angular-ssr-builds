fed33fd48a43c1bb5bc371240dd7b28d2089752c
Angular Builds <dev-infra@angular.dev>
2026-06-01 07:33:26 +0000

    2c0dfc2 fix(@angular/ssr): add support for configuring trusted proxy headers via environment variable

--- fesm2022/node.mjs
+++ fesm2022/node.mjs
@@ -12,19 +12,28 @@ import { argv } from 'node:process';
  * @returns An array of allowed hosts.
  */
 function getAllowedHostsFromEnv() {
-    const allowedHosts = [];
-    const envNgAllowedHosts = process.env['NG_ALLOWED_HOSTS'];
-    if (!envNgAllowedHosts) {
-        return allowedHosts;
+    return getArrayFromEnv('NG_ALLOWED_HOSTS');
+}
+/**
+ * Retrieves the list of trusted proxy headers from the environment variable `NG_TRUST_PROXY_HEADERS`.
+ * @returns An array of trusted proxy headers.
+ */
+function getTrustProxyHeadersFromEnv() {
+    return getArrayFromEnv('NG_TRUST_PROXY_HEADERS');
+}
+function getArrayFromEnv(envName) {
+    const envValue = process.env[envName];
+    if (!envValue) {
+        return undefined;
     }
-    const hosts = envNgAllowedHosts.split(',');
-    for (const host of hosts) {
-        const trimmed = host.trim();
+    const values = [];
+    for (const value of envValue.split(',')) {
+        const trimmed = value.trim();
         if (trimmed.length > 0) {
-            allowedHosts.push(trimmed);
+            values.push(trimmed);
         }
     }
-    return allowedHosts;
+    return values;
 }
 
 class CommonEngineInlineCriticalCssProcessor {
@@ -98,10 +107,7 @@ class CommonEngine {
     allowedHosts;
     constructor(options) {
         this.options = options;
-        this.allowedHosts = new Set([
-            ...getAllowedHostsFromEnv(),
-            ...(this.options?.allowedHosts ?? []),
-        ]);
+        this.allowedHosts = new Set(getAllowedHostsFromEnv() ?? this.options?.allowedHosts ?? []);
     }
     /**
      * Render an HTML document for a specific URL with specified
@@ -367,11 +373,13 @@ class AngularNodeAppEngine {
      * @param options Options for the Angular Node.js server application engine.
      */
     constructor(options) {
-        this.angularAppEngine = new AngularAppEngine({
+        const appEngineOptions = {
             ...options,
-            allowedHosts: [...getAllowedHostsFromEnv(), ...(options?.allowedHosts ?? [])],
-        });
-        this.trustProxyHeaders = options?.trustProxyHeaders;
+            allowedHosts: getAllowedHostsFromEnv() ?? options?.allowedHosts,
+            trustProxyHeaders: getTrustProxyHeadersFromEnv() ?? options?.trustProxyHeaders,
+        };
+        this.angularAppEngine = new AngularAppEngine(appEngineOptions);
+        this.trustProxyHeaders = appEngineOptions.trustProxyHeaders;
     }
     /**
      * Handles an incoming HTTP request by serving prerendered content, performing server-side rendering,
